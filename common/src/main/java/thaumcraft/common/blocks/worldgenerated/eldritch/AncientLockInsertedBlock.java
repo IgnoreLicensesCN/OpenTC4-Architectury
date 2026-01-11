17185b3942e0d75fd3a082deb579aa00ca8c6468
AcdeAsdff <acdeasdff@gmail.com>
2026-01-12 00:17:59 +0800

    ancient lock(not finished since i haven't work in outer generation and entity classes)

--- common/src/main/java/thaumcraft/common/blocks/worldgenerated/eldritch/AncientLockInsertedBlock.java
+++ common/src/main/java/thaumcraft/common/blocks/worldgenerated/eldritch/AncientLockInsertedBlock.java
@@ -1,18 +1,19 @@
 package thaumcraft.common.blocks.worldgenerated.eldritch;
 
+import dev.architectury.platform.Platform;
+import dev.architectury.utils.Env;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.material.MapColor;
 import org.jetbrains.annotations.Nullable;
+import thaumcraft.client.fx.migrated.particles.FXSpark;
 import thaumcraft.common.tiles.ThaumcraftBlockEntities;
 import thaumcraft.common.tiles.eldritch.AncientLockInsertedBlockEntity;
 
@@ -39,5 +40,22 @@ public class AncientLockInsertedBlock extends AncientLockEmptyBlock implements E
                 lock.tick();
             }
         };
+    }
+
+    @Override
+    public void animateTick(BlockState blockState, Level level, BlockPos blockPos, RandomSource randomSource) {
+        if (Platform.getEnvironment() != Env.CLIENT) {
+            return;
+        }
+        if (!(level instanceof ClientLevel clientLevel)){
+            return;
+        }
+        final int i = blockPos.getX();
+        final int j = blockPos.getY();
+        final int k = blockPos.getZ();
+        FXSpark ef = new FXSpark(clientLevel, (float)i + level.random.nextFloat(), (float)j + level.random.nextFloat(), (float)k + level.random.nextFloat(), 0.5F);
+        ef.setRBGColorF(0.65F + level.random.nextFloat() * 0.1F, 1.0F, 1.0F);
+        ef.setAlphaF(0.8F);
+        Minecraft.getInstance().particleEngine.add(ef);
     }
 }
